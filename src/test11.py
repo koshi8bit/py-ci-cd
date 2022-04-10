4ec58e1e2b1cd33d04bc727a8ce6ed8f6f5efdb9
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2022-04-10 14:07:22 +0700

    args print

--- src/test11.py
+++ src/test11.py
@@ -3,7 +3,9 @@ import sys
 
 # exit()
 sep = "\n"
-for b in sys.argv[1:]:
+args = sys.argv[1:]
+print("Args:", args)
+for b in args:
     print(f"\n{b}")
     val = os.environ[b]
     if sep in val:
