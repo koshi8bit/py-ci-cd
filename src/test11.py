58ca82dbbbe7a51a9e69e2bbdeab36d057e4dfbb
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2022-04-10 11:41:20 +0700

    manual

--- src/test11.py
+++ src/test11.py
@@ -3,6 +3,7 @@ a = ['SERVER_SSH_PRIV_KEY', 'SERVER_SSH_USER', 'SERVER_ADDRES']
 # exit()
 sep = "\n"
 for b in a:
+    print(f"\n{b}\n")
     val = os.environ[b]
     if sep in val:
         lines = val.split(sep)
