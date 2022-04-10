0bdb658bc6b8d5f24a847249cc89a5165667dfe7
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2022-04-10 11:26:23 +0700

    formatting

--- src/test11.py
+++ src/test11.py
@@ -1,12 +1,11 @@
 import os
 a = ['SERVER_SSH_PRIV_KEY', 'SERVER_SSH_USER', 'SERVER_ADDRES']
+exit()
 sep = "\n"
 for b in a:
     val = os.environ[b]
-    print(sep in val)
     if sep in val:
         lines = val.split(sep)
-        print(lines)
         lines = list(map(lambda x: x[:-1], lines))
         for line in lines:
             print(line)
