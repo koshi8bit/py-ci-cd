d1342234353009d50a4991801f1dc5238f853b9f
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2022-04-13 21:44:59 +0700

    readme fix

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # if more than 120 symbols len -> Lint fail
+    # if more than 120 symbols len -> Lint (code format guidelines check) fail
     a = "long_line long_line long_line long_line long_line long_line long_line long_line long_line long_line  " \
         "long_line long_line long_line long_line long_line "
     print(a)
@@ -15,6 +15,11 @@ def index():
 @app.route("/api/v1/foo")
 def api_v1_foo():
     return "bar"
+
+
+@app.route("/api/v2/foo")
+def api_v2_foo():
+    return "sexy bar"
 
 
 if __name__ == '__main__':
