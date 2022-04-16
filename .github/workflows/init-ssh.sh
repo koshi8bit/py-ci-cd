7e313b109945ca82dd37e6ee3e5ac98e1ae16968
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2022-04-16 13:02:58 +0700

    sh

---
+++ .github/workflows/init-ssh.sh
@@ -0,0 +1,11 @@
+SSH_PRIVATE_KEY="$SERVER_SSH_PRIV_KEY"
+SSH_KNOWN_HOSTS="$SERVER_ADDRES"
+test -n "$SSH_PRIVATE_KEY" || ( echo "missing variable SSH_PRIVATE_KEY" && exit 1)
+test -n "$SSH_KNOWN_HOSTS" || ( echo "missing variable SSH_KNOWN_HOSTS" && exit 1)
+which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
+eval $(ssh-agent -s)
+echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+ssh-keyscan -H "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
